294a00148cd089b9a51a94ec7cc51134f1b56ab7
AhuvaEhren <57751062+AhuvaEhren@users.noreply.github.com>
2019-11-30 22:54:17 +0200

    Memory Game with Updates
    Button has been updated as well as modal

--- Memory Game/js/memory game.js
+++ Memory Game/js/memory game.js
@@ -7,22 +7,24 @@ memory_game.hasFlippedCard = false;
 memory_game.lockCards = false;
 memory_game.matchesCounter = 0;
 
+memory_game.newGameBtnHead.addEventListener("click", startGame);
+
 //Fisher-Yates shuffle
-window.onload = (memory_game.shuffleCards = function(){
+window.onload = (memory_game.shuffleCards = function () {
     memory_game.cards.forEach(card => {
-        memory_game.randomizingCardPosition = Math.floor(Math.random() * 12); 
+        memory_game.randomizingCardPosition = Math.floor(Math.random() * 12);
         card.style.order = memory_game.randomizingCardPosition;
     });
 });
 
 memory_game.flipCard = function () {
-    if(memory_game.lockCards){
+    if (memory_game.lockCards) {
         return;
     };
-    if(this === memory_game.firstCard){
+    if (this === memory_game.firstCard) {
         return;
     };
-    
+
     this.classList.add("flip");
 
     if (!memory_game.hasFlippedCard) {
@@ -46,58 +48,57 @@ memory_game.matching = function () {
         memory_game.firstCard.removeEventListener("click", memory_game.flipCard);
         memory_game.secondCard.removeEventListener("click", memory_game.flipCard);
 
-        memory_game.matchesCounter +=1;
-        setTimeout(()=>{
-        memory_game.congratulations()}, 1200);
+        memory_game.matchesCounter += 1;
+        setTimeout(() => {
+            memory_game.congratulations()
+        }, 1200);
     }
     // if cards don't match, unflip cards
     else {
         memory_game.lockCards = true;
-        setTimeout( function(){
-        memory_game.firstCard.classList.remove("flip");
-        memory_game.secondCard.classList.remove("flip");
-        memory_game.lockCards = false;
+        setTimeout(function () {
+            memory_game.firstCard.classList.remove("flip");
+            memory_game.secondCard.classList.remove("flip");
+            memory_game.lockCards = false;
 
-        memory_game.resetUnmatchedCards();
-        }, 1000 );
+            memory_game.resetUnmatchedCards();
+        }, 1000);
     }
 
 };
 
 // reset unmatched cards at the end of each round 
-memory_game.resetUnmatchedCards = function(){
-    memory_game.hasFlippedCard = false; 
+memory_game.resetUnmatchedCards = function () {
+    memory_game.hasFlippedCard = false;
     memory_game.lockCards = false;
     memory_game.firstCard = null;
     memory_game.secondCard = null;
 };
 
-memory_game.congratulations = function(){
-    if(memory_game.matchesCounter === 6 ){
-        setTimeout(()=> {memory_game.modal.classList.remove("display-none");},1000);
+memory_game.congratulations = function () {
+    if (memory_game.matchesCounter === 6) {
+        setTimeout(() => { memory_game.modal.classList.remove("display-none"); }, 1000);
     };
     memory_game.newGameBtnModal.addEventListener("click", startGame);
 };
 
-memory_game.newGameBtnHead.addEventListener("click", startGame);
-
-function startGame(){
-     memory_game.cards.forEach(card => {
-         if(card.classList.contains("flip")){
-             card.classList.remove("flip");
-             memory_game.hasFlippedCard = false;
-             memory_game.lockCards = false;
-             memory_game.cards.forEach(card => card.addEventListener("click", memory_game.flipCard));
-             memory_game.resetUnmatchedCards();
-             memory_game.matchesCounter = 0;
-         }
-     })
-     setTimeout(() => { 
+function startGame() {
+    memory_game.cards.forEach(card => {
+        if (card.classList.contains("flip")) {
+            card.classList.remove("flip");
+            memory_game.hasFlippedCard = false;
+            memory_game.lockCards = false;
+            memory_game.cards.forEach(card => card.addEventListener("click", memory_game.flipCard));
+            memory_game.resetUnmatchedCards();
+            memory_game.matchesCounter = 0;
+        }
+    })
+    setTimeout(() => {
         memory_game.shuffleCards()
     }, 1000);
-     memory_game.modal.classList.add("display-none");
+    memory_game.modal.classList.add("display-none");
 
-    };
+};
 
 
 
